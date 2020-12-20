ed8ce82a8b724ca45157be55f0fe27179a75f689
nggerber <gerberclimbs@gmail.com>
2020-12-20 14:36:59 -0700

    created api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,55 @@
 const express = require("express");
 const router = express.Router();
-const Workout = require("../")
+const Workout = require("../models/workout")
+
+//GET Route to find workouts
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        });
+});
+
+//POST route to create workouts
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err)
+        });
+});
+
+//get route for range
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        });
+});
+
+//update workouts based on id
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate(
+        { _id: req.poarams.id },
+        { $push: { excercises: req.body } },
+        { new: true }
+
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
